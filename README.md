8c00e0789101ca8ca632e8f84b244ade56ff3f15
kshvsharma960 <kshvsharma960@gmail.com>
2018-10-21 13:30:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# rep
-My First Repository
+# flyme
---
+++ resume.js
@@ -0,0 +1,7 @@
+$(".yearExp").html("3 years");
+
+function reloadGame(){
+var iframe = document.getElementById("game");
+
+iframe.src = iframe.src;
+}
